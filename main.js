46bd3c533ea8c05342e49ad5a69f6db3af1875b1
Brianne Hostutler <briannehostutler@gmail.com>
2016-04-21 20:09:34 -0400

    added students

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ var prompt = require("prompt");
 
 var Bus = require("./bus.js");
 
-
 prompt.start();
 
-var myBus = new Bus("Michael", "red", "full");
-var myBus2 = new Bus("Michael", "green", "full");
+var myBus = new Bus("Mary", "red", "full");
 
-console.log(myBus);
-console.log(myBus2);
-
-myBus.studentEntersBus("Owens","M","B",3.5,14,true,"Hey bud, let's party");
-myBus2.studentEntersBus("Ruby","F","A",4.1,4,true,"Haaayyy");
-
-
-console.log(myBus);
-console.log(myBus2);
+myBus.studentEntersBus("Carl","M","B",3.0,1,true,"Hi");
+myBus.studentEntersBus("Ellie","F","B",3.5,2,true,"Hello");
+myBus.studentEntersBus("John","M","C",2.5,11,true,"NOOO");
+myBus.studentEntersBus("Cinderella","F","A",3.5,2,true,"Hey");
+myBus.studentEntersBus("Bob","M","F",0.5,25,true,"ugh");
+myBus.studentEntersBus("Rapunzel","F","B",3.5,2,true,"Yay");
